61416933c4e50bc891952d2b7135e3e79cf38339
Mark Eschbach <meschbach@gmail.com>
2019-05-12 16:12:49 -0700

    init: First capture of somewhat working setup

---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Clean up existing configurations
+rm -fR root servers clients
---
+++ generate.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Verify we have cfssl
+lines=$(which cfssl |wc -l)
+if [ $lines -lt 1 ] ; then
+    echo "cfssl not found"
+    exit -1
+fi
+
+# Establish the environment
+base=$(realpath $(dirname $0))
+echo "Using '$base' for base"
+reqs="$base"
+data="$base"
+
+# Clean up existing configurations
+./clean.sh
+
+
+# Create a new root CA
+mkdir -p root
+( cd root
+echo "================================"
+echo "Generating Root CA"
+echo "================================"
+cfssl gencert -initca ../ca-sr.json | cfssljson -bare ca
+)
+
+# Create our server CA
+mkdir -p servers
+( cd servers
+echo "================================"
+echo "Generating Server CA"
+echo "================================"
+cfssl genkey -initca $reqs/servers-sr.json | cfssljson -bare servers
+
+echo ">> Signing with CA"
+cfssl sign -ca $data/root/ca.pem -ca-key $data/root/ca-key.pem --config $reqs/intermediate-config.json -profile intermediate servers.csr |cfssljson -bare servers
+
+echo "================================"
+echo "Generating test server certificate"
+echo "================================"
+cfssl gencert -ca=$data/servers/servers.pem -ca-key=$data/servers/servers-key.pem -config $reqs/leafs-config.json -profile server $reqs/server-test.json |cfssljson -bare server-test
+
+)
+
+# Create our server CA
+mkdir -p clients
+( cd clients
+echo "================================"
+echo "Generating Clients CA"
+echo "================================"
+cfssl genkey -initca $reqs/clients-sr.json | cfssljson -bare clients
+
+echo ">> Signing with CA"
+cfssl sign -ca $data/root/ca.pem -ca-key $data/root/ca-key.pem --config $reqs/intermediate-config.json -profile intermediate clients.csr |cfssljson -bare clients
+)
